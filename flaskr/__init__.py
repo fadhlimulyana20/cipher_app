122c2b19a08d9b033fad2a8922494f6060e11012
Fadhli Mulyana <baba.fadhli@gmail.com>
2021-09-30 03:17:54 +0700

    Menambahkan substitution cipher pada web app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
+from re import sub
 from flask import Flask, render_template, request
-from decrypt import decrypt
-from encrypt import encrypt
+from . import substitution, shift
 
 
 def create_app(test_config=None):
@@ -26,42 +26,9 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/', methods=('GET', 'POST'))
-    def index():
-        data = {
-            "mode": "encrypt",
-            "input_shift": "",
-            "input_text": "",
-            "output_text": "",
-            "output_style": "1"
-        }
 
-        if request.method == 'POST':
-            mode = request.form['mode']
-            input_text = request.form['input_text']
-            shift = int(request.form['input_shift'])
-            output_style = request.form['output_style']
 
-            data['mode'] = mode
-            data['input_text'] = input_text
-            data['input_shift'] = shift
-            data['output_style'] = output_style
-
-            if mode == "encrypt":
-                if (output_style == "1"):
-                    data['output_text'] = encrypt(
-                        input_text, shift).replace(" ", "")
-                else:
-                    new_str = encrypt(input_text, shift).replace(" ", "")
-
-                    data['output_text'] = ' '.join(
-                        new_str[i:i+5] for i in range(0, len(new_str), 5))
-            else:
-                data['output_text'] = decrypt(
-                    input_text, shift).replace(" ", "")
-
-            return render_template('index.html', data=data)
-
-        return render_template('index.html', data=data)
+    app.register_blueprint(substitution.bp)
+    app.register_blueprint(shift.bp)
 
     return app
---
+++ flaskr/shift.py
@@ -0,0 +1,43 @@
+from flask import (Blueprint, request, render_template)
+from decrypt import decrypt
+from encrypt import encrypt
+
+bp = Blueprint('shift', __name__, url_prefix='/shift')
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    data = {
+        "mode": "encrypt",
+        "input_shift": "",
+        "input_text": "",
+        "output_text": "",
+        "output_style": "1"
+    }
+
+    if request.method == 'POST':
+        mode = request.form['mode']
+        input_text = request.form['input_text']
+        shift = int(request.form['input_shift'])
+        output_style = request.form['output_style']
+
+        data['mode'] = mode
+        data['input_text'] = input_text
+        data['input_shift'] = shift
+        data['output_style'] = output_style
+
+        if mode == "encrypt":
+            if (output_style == "1"):
+                data['output_text'] = encrypt(
+                    input_text, shift).replace(" ", "")
+            else:
+                new_str = encrypt(input_text, shift).replace(" ", "")
+
+                data['output_text'] = ' '.join(
+                    new_str[i:i+5] for i in range(0, len(new_str), 5))
+        else:
+            data['output_text'] = decrypt(
+                input_text, shift).replace(" ", "")
+
+        return render_template('index.html', data=data)
+
+    return render_template('index.html', data=data)
